8919a4c1599f7d58c48e58dc186cad5c8da1bb39
Igor Oliveira dos Santos <igorsantos381@gmail.com>
2024-09-08 16:39:35 -0300

    creating signup method and integration with AWS Cognito
    better syntax for grabbing properties from event body
    
    comment mock code

---
+++ src/signup/index.ts
@@ -0,0 +1,46 @@
+import {
+  CognitoIdentityProviderClient,
+  SignUpCommand,
+  SignUpCommandOutput,
+} from "@aws-sdk/client-cognito-identity-provider";
+import { getSecretValue } from "../util";
+
+const client = new CognitoIdentityProviderClient({
+  region: "us-east-1",
+});
+
+export const handleSignup = async (event: { body: any }) => {
+  const { username, password, email } = event.body;
+
+  const secretHash = getSecretValue(
+    username,
+    process.env.COGNITO_CLIENT_ID as string,
+    process.env.COGNITO_CLIENT_SECRET as string
+  );
+  const singupCommand = new SignUpCommand({
+    ClientId: process.env.COGNITO_CLIENT_ID,
+    SecretHash: secretHash,
+    Username: username,
+    Password: password,
+    UserAttributes: [
+      // AttributeListType
+      {
+        // AttributeType
+        Name: "email", // required
+        Value: email,
+      },
+    ],
+  });
+  try {
+    const response = await client.send(singupCommand)
+    return {
+      statusCode: 201,
+      body: JSON.stringify(response),
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error),
+    }
+  }
+};
